8123bc7ee368e738102881751112c221ac21d80e
Your Name <you@example.com>
2023-04-16 19:33:32 -0300

    build test

--- src/app/dashboard/dashboard-components/spinner-dialog-component/progress-spinner-dialog.component.ts
+++ src/app/dashboard/dashboard-components/spinner-dialog-component/progress-spinner-dialog.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-progress-spinner-dialog',
-  templateUrl: './progress-spinner-dialog.component.html',
-  styleUrls: ['./progress-spinner-dialog.component.css']
+  templateUrl: './progress-spinner-dialog.component.html'
 })
 export class ProgressSpinnerDialogComponent implements OnInit {
 
